8084b25d8068fef17757111c941b31db7b2977d7
Marcel J Merchat <merchat77@gmail.com>
2017-07-14 13:14:08 -0500

    This is the part of the initial setup.
    The user key is inside the file hidden.py.

---
+++ hidden.py
@@ -0,0 +1,23 @@
+# Keep this file separate
+
+def oauth() :
+    return { "consumer_key" : "w8no5NRKrVidIwP8usd5PEgMv",
+        "consumer_secret" : "FhkKocmdqlbAqGy4ZEV1laUj4jIKkmRekHi2Lw3wO8wcCNdcOr",
+        "token_key" : "",
+        "token_secret" : "" }
+        
+def get_app_key() :
+    return ("w8no5NRKrVidIwP8usd5PEgMv")
+def get_app_secret() :
+    return ("FhkKocmdqlbAqGy4ZEV1laUj4jIKkmRekHi2Lw3wO8wcCNdcOr")
+#def get_token_key() :
+#   return ("")    
+#def get_token_secret() :
+#    return ("")
+        
+## myFirstTwitterAPI_MarcelMerchat
+## Twitter Account 544800935
+
+# https://github.com/marcelMerchat/twitterAPIinPython.git
+# myFirstTwitterAPI_MarcelMerchat2
+# 544800935
---
+++ twurl.py
@@ -0,0 +1,28 @@
+import urllib
+import oauth
+import hidden
+
+def augment(url, parameters) :
+    secrets = hidden.oauth()
+    consumer = oauth.OAuthConsumer(secrets['consumer_key'], secrets['consumer_secret'])
+    token = oauth.OAuthToken(secrets['token_key'],secrets['token_secret'])
+
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, 
+        token=token, http_method='GET', http_url=url, parameters=parameters)
+    oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, token)
+    return oauth_request.to_url()
+
+
+def test_me() :
+    print ('* Calling Twitter...')
+    url = augment('https://api.twitter.com/1.1/statuses/user_timeline.json',
+        {'screen_name': 'MarcelMerchat', 'count': '2'} )
+    print (url)
+    connection = urllib.urlopen(url)
+    data = connection.read()
+    print (data)
+    headers = connection.info().dict
+    print (headers)
+    
+def get_app_key() :
+    return ("y4bG0Sr3AuCZvqOUMtTDEQUkX")
